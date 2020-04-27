4839e69f432675518390077fc87758bef8fba9db
jjosburn <jjosburn@gmail.com>
2020-04-26 23:34:23 -0500

    this is really carlisp with no eval

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 //inspired by https://github.com/carld/homelisp
-#include <iostream>
-#include <list>
-#include <map>
-#include <stack>
-#include <string>
-#include <vector>
-#include <fstream>
-#include <sstream>
-#include <exception>
+/* reads a symbolic expression into memory
+ * Copyright (C) 2015 A. Carl Douglas
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cstring>
 
+ /* structures and functions for representing symbolic expressions .
+  * an object structure can store either an atomic type:
+  *   a symbol or a number, or a Cons cell.
+  *
+  * copyright (C) 2015 A. Carl Douglas
+  */
+#include <stdlib.h>
 enum { PAIR = 1, NUMBER = 2, SYMBOL = 3, OPERATOR = 4, POINTER = 5, STRING = 6 };
 typedef struct object OBJECT;
 typedef OBJECT * (prim_op)(OBJECT *, OBJECT *);
@@ -48,23 +53,67 @@ extern OBJECT _FALSE;
 #define _rplaca(x,y)    (x->value.pair.car = y)
 #define _rplacd(x,y)    (x->value.pair.cdr = y)
 
-typedef struct object OBJECT;
+#define _bind(ex,va,en)    _cons(_cons(ex, _cons(va, NIL)), en)
+
+OBJECT * _object_malloc(int type);
+OBJECT * _cons(OBJECT *car, OBJECT *cdr);
+
+/* in place reverse */
+OBJECT * _reverse_in_place(OBJECT *expr);
+
+/*
+ * (def assoc (x y)
+ *   (cond ((eq (caar y) x) (cadar y))
+ *           (true (assoc x (cdr y)))))
+ */
+OBJECT * _lookup(OBJECT *expr /* x */, OBJECT *env /* y */);
+
+OBJECT * make_number_i(int integer);
+
+OBJECT * make_number(const char *token);
+
+OBJECT * make_primitive(prim_op *pp);
+
+/* this helps passing around FILE * etc */
+OBJECT * make_pointer(void * ptr);
+
+OBJECT * make_symbol(const char *symbol);
+
+/* if the length parameter is less than the length of
+ * the string the actual string length will be used
+ */
+OBJECT * make_string(const char *str, size_t length);
+
+/* warning this mutates the first parameter */
+OBJECT *_append(OBJECT *exp1, OBJECT *exp2);
+
+const char * _strcat_alloc(const char *str1, const char *str2);
+OBJECT *string_cat(OBJECT *, OBJECT *);
+
+OBJECT * _evlis(OBJECT *expr, OBJECT *environ);
+
+OBJECT * _eval(OBJECT *expr, OBJECT *environ);
+
+OBJECT * _read(OBJECT *port, int eof_exit);
+
+OBJECT * _print(OBJECT *exp, OBJECT *);
+
+OBJECT * debug(OBJECT *exp);
+
+#define debugf(x,m) printf("%s:%d --- %s\n", __FILE__, __LINE__, m); debug(x)
 
 enum { T_NONE = -1, T_LPAREN = 0, T_RPAREN = 1, T_SYMBOL = 2, T_NUMBER = 3, T_STRING = 4, T_QUOTE = 5, T_COMMENT = 6 };
 
-OBJECT _NIL = { PAIR,   { NULL } };
-OBJECT _TRUE = { SYMBOL, { "#t" } };
-OBJECT _FALSE = { SYMBOL, { "#f" } };
-
-int get_token(std::istream& in,std::string &token, int exit_on_eof)
-{
+/* returns token type on success, -1 on error or EOF
+ * lexer states and transitions are in the diagram lexer-state.dot */
+int get_token(FILE *fp, char *token, int exit_on_eof) {
 	int state = T_NONE;
 	int ch;
 top:
-	ch = in.get();
+	ch = fgetc(fp);
 	if (ch == EOF) {
 		if (exit_on_eof == 1) {
-			std::cout << "Exiting" << std::endl;
+			printf("Exiting.\n");
 			exit(0);
 		}
 		return T_NONE;
@@ -74,113 +123,84 @@ top:
 		if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r') { /* ignore leading whitespace */
 		}
 		else if (ch == '(') {
-			token.clear();
-			token.push_back(ch);
-			return T_LPAREN;
+			*token++ = ch; *token = '\0'; return T_LPAREN;
 		}
 		else if (ch == ')') {
-			token.clear();
-			token.push_back(ch);
-			return T_RPAREN;
+			*token++ = ch; *token = '\0'; return T_RPAREN;
 		}
-		else if (ch == '\'') 
-		{
-			token.clear();
-			token.push_back(ch);
-			return T_QUOTE;
+		else if (ch == '\'') {
+			*token++ = ch; *token = '\0'; return T_QUOTE;
 		}
 		else if (ch == ';') {
 			state = T_COMMENT;
 		}
-		else if (std::strchr("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_:-+=*&^%$#@!~'<>/?`|", ch)) {
-			token.clear();
-			token.push_back(ch);
+		else if (strchr("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_:-+=*&^%$#@!~'<>/?`|", ch)) {
+			*token++ = ch; *token = '\0';
 			state = T_SYMBOL;
-		}
-		else if (std::strchr("0123456789", ch)) {
-			token.clear();
-			token.push_back(ch);
+		}   
+		else if (strchr("0123456789", ch)) {
+			*token++ = ch; *token = '\0';
 			state = T_NUMBER;
 		}
 		else if (ch == '"') {
 			state = T_STRING;
 		}
 		else {
-			std::cout << "Error, unexpected" << ch << state << std::endl;
-			//printf("Error, unexpected '%c' in state %d\n", ch, state);
+			printf("Error, unexpected '%c' in state %d\n", ch, state);
 			exit(1);
 		}
 		break;
 	case T_COMMENT:
-		if (std::strchr("\r\n", ch) /* hit a new line */) {
+		if (strchr("\r\n", ch) /* hit a new line */) {
 			state = T_NONE;
 		}
 		break;
 	case T_SYMBOL:
-		if (std::strchr("()' \t\r\n", ch) /* hit whitespace or syntax */) {
-			//ungetc(ch, fp);
-			in.unget();
+		if (strchr("()' \t\r\n", ch) /* hit whitespace or syntax */) {
+			ungetc(ch, fp);
 			return T_SYMBOL;
 		}
-		token.push_back(ch);
+		*token++ = ch; *token = '\0';
 		break;
 	case T_NUMBER:
-		if (strchr("0123456789", ch) == NULL /* not a number */) 
-		{
-			in.unget();
+		if (strchr("0123456789", ch) == NULL /* not a number */) {
+			ungetc(ch, fp);
 			return T_NUMBER;
 		}
-		token.push_back(ch);
+		*token++ = ch; *token = '\0';
 		break;
 	case T_STRING:
-		if (ch == '"') 
-		{
-			token.clear();
+		if (ch == '"') {
+			*token = '\0';
 			return T_STRING;
 		}
-		token.push_back(ch);
+		*token++ = ch; *token = '\0';
 		break;
 	}
 	goto top;
 }
 
-OBJECT * _object_malloc(int type) 
-{
-	OBJECT *obj = static_cast<OBJECT *>(malloc(sizeof(OBJECT)));
-	obj->type = type; return obj;
-}
 
-OBJECT * _cons(OBJECT *car, OBJECT *cdr) 
-{
-	OBJECT *obj = _object_malloc(PAIR);
-	obj->value.pair.car = car;
-	obj->value.pair.cdr = cdr;
+OBJECT * make_symbol(const char *symbol) {
+	OBJECT *obj = NULL; // _interned_syms;
+	//size_t len = strlen(symbol) + 1;
+	//char * storage = 0;
+	//for (; obj != NIL; obj = _cdr(obj)) {
+	//	if (strcmp(symbol, symbol_name(_car(obj))) == 0) {
+	//		return _car(obj);
+	//	}
+	//}
+	//storage = GC_MALLOC(len);
+	//memcpy(storage, symbol, len);
+	//obj = _object_malloc(SYMBOL);
+	//obj->value.symbol = storage;
+	//obj->size = len;
+	//_interned_syms = _cons(obj, _interned_syms);
 	return obj;
 }
 
-OBJECT * make_string(const std::string & token, size_t length) 
-{
-	OBJECT *obj = _object_malloc(STRING);
-	size_t len = strlen(token.c_str()) + 1;
-	obj->size = length > len ? length : len;
-	obj->value.string = static_cast<char*>(malloc(obj->size));
-	memcpy(obj->value.string, token.c_str(), len);
-	return obj;
-}
-
-
-OBJECT * make_number(const std::string & token) 
-{
-	OBJECT *obj = _object_malloc(NUMBER);
-
-	obj->value.number.integer = atoi(token.c_str());
-	return obj;
-}
-
-OBJECT * _append(OBJECT *exp1, OBJECT *exp2) 
-{
-	if (exp1 != NIL) 
-	{
+OBJECT * _append(OBJECT *exp1, OBJECT *exp2) {
+	if (exp1 != NIL) {
 		OBJECT *tmp = exp1;
 		for (; _cdr(tmp) != NIL; tmp = _cdr(tmp))
 			; /* walk to end of list */
@@ -190,21 +210,18 @@ OBJECT * _append(OBJECT *exp1, OBJECT *exp2)
 	return exp2;
 }
 
-OBJECT * _read(std::istream& in, int eof_exit) 
-{
+OBJECT * _read(OBJECT *port, int eof_exit) {
 	OBJECT *expr_stack = NIL;
 	OBJECT *quote_stack = NIL;
 	OBJECT *expr = NIL;
 	OBJECT *obj = NIL;
-	//char token[80];
-	std::string token;
+	char token[80];
 	int tok_type;
-	for (; ; ) 
-	{
-		tok_type = get_token(in, token, eof_exit);
+	for (; ; ) {
+		tok_type = get_token((FILE *)pointer(port), token, eof_exit);
 		if (tok_type < 0) break; /* EOF */
 		if (tok_type == T_LPAREN) 
-		{ 
+		{
 			expr_stack = _cons(expr, expr_stack); /* remember the current list */
 			expr = NIL; /* start a new list */
 			continue; /* go to top and read the first object in the expression */
@@ -225,181 +242,117 @@ OBJECT * _read(std::istream& in, int eof_exit)
 		}
 		else if (tok_type == T_SYMBOL) 
 		{
-			obj = _cons(make_string(token,0), NIL);
+			obj = _cons(make_symbol(token), NIL);
 		}
-		else if (tok_type == T_QUOTE) 
-		{
-			exit(-1);
-		}
-		else 
-		{
-			std::cout << "unrecognised token:" << token << std::endl; 
-			exit(1);
-		}
+		//else if (tok_type == T_QUOTE) {
+		//	obj = _cons(_cons(make_symbol("quote"), NIL), NIL); /* insert quote procedure call */
+		//	quote_stack = _cons(obj, quote_stack); /* keep track of quotes */
+		//	expr = _append(expr, obj); /* append (quote) to the current expression */
+		//	continue; /* go to the top and read the quoted object */
+		//}
+		//else {
+		//	printf("unrecognised token: '%s'\n", token); exit(1);
+		//}
 		expr = _append(expr, obj); /* append the object to the current expression */
 		if (expr_stack == NIL) /* an expression as been read */
 			break;
 	}
+	/*  finish expanding quotes: a 'b c  ->  a (quote b) c  ...  a '(b) c  ->  a (quote (b)) c   */
+	//for (; quote_stack != NIL; quote_stack = _cdr(quote_stack)) {
+	//	_rplacd(_car(_car(quote_stack)), _cdr(_car(quote_stack))); /* a (quote) b c  ->  a (quote b) c */
+	//	_rplacd(_car(quote_stack), _cdr(_cdr(_car(quote_stack))));
+	//	_rplacd(_cdr(_car(quote_stack)), NIL);
+	//}
 	return expr != NIL ? _car(expr) : NIL;
 }
-struct SExp {
-	SExp() : type(LIST) {}
-	SExp(const std::list<SExp>& l) : type(LIST), list(l) {}
-	SExp(const std::string&     s) : type(ATOM), atom(s) {}
-	enum { ATOM, LIST } type;
-	std::string         atom;
-	std::list<SExp>     list;
-};
 
-  
-static SExp
-readExpression(std::istream& in)
-{
-	std::stack<SExp> stk;
-	std::string      tok;
+OBJECT _NIL = { PAIR,   { NULL } };
+OBJECT _TRUE = { SYMBOL, { "#t" } };
+OBJECT _FALSE = { SYMBOL, { "#f" } };
 
-#define APPEND_TOK() \
-	if (stk.empty()) return tok; else stk.top().list.push_back(SExp(tok))
+OBJECT *_interned_syms = NIL;
 
-	while (char ch = in.get()) {
-		switch (ch) {
-		case EOF:
-			return SExp();
-		case ' ': case '\t': case '\n':
-			if (tok == "")
-				continue;
-			else
-				APPEND_TOK();
-			tok = "";
-			break;
-		case '"':
-			do { tok.push_back(ch); } while ((ch = in.get()) != '"');
-			tok.push_back('"');
-			APPEND_TOK();
-			tok = "";
-			break;
-		case '(':
-			stk.push(SExp());
-			break;
-		case ')':
-			switch (stk.size()) {
-			case 0:
-				throw std::exception("Missing '('");
-				break;
-			case 1:
-				if (tok != "") stk.top().list.push_back(SExp(tok));
-				return stk.top();
-			default:
-				if (tok != "") stk.top().list.push_back(SExp(tok));
-				SExp l = stk.top();
-				stk.pop();
-				stk.top().list.push_back(l);
-			}
-			tok = "";
-			break;
-		default:
-			tok.push_back(ch);
-		}
-	}
-
-	switch (stk.size()) {
-	case 0:  
-		return tok;       break;
-	case 1:  
-		return stk.top(); 
-		break;
-	default: throw std::exception("Missing ')'");
-	}
+OBJECT * _object_malloc(int type) {
+	OBJECT *obj = static_cast<OBJECT *>(malloc(sizeof(OBJECT)));
+	obj->type = type; return obj;
 }
 
-void print(std::ostream &out, SExp &_sexp)
-{
-	if (_sexp.type == SExp::LIST)
-	{
-		std::list<SExp>::iterator it;
-		out << "(";
-		for (it = _sexp.list.begin(); it != _sexp.list.end(); it++)
-		{
-			auto foo = *it;
-			if (foo.type == SExp::ATOM)
-			{
-				out << foo.atom << " ";
-			}
-			else
-			{ 
-				print(out, foo);
-			}
-			
-		}
-		out << ") ";
-	}
-	else if (_sexp.type == SExp::ATOM)
-	{
-		out << _sexp.atom << " ";
-	}
+OBJECT * _cons(OBJECT *car, OBJECT *cdr) {
+	OBJECT *obj = _object_malloc(PAIR);
+	obj->value.pair.car = car;
+	obj->value.pair.cdr = cdr;
+	return obj;
 }
 
-std::ostream &operator<<(std::ostream &os, OBJECT *obj)
-{
-    if (obj == NIL) {
-		//snprintf(str, 255, "[%p NIL]", (void *)obj);
-		os  << "[" << (void*) obj << "]";
-		return os;
+/* in place reverse */
+OBJECT * _reverse_in_place(OBJECT *expr) {
+	OBJECT *tmp, *revexpr = NIL;
+	while (expr != NIL) {
+		tmp = _cdr(expr);
+		_rplacd(expr, revexpr);
+		revexpr = expr;
+		expr = tmp;
 	}
+	return revexpr;
+}
 
+OBJECT * make_number(const char *token) {
+	OBJECT *obj = _object_malloc(NUMBER);
+	char *dec = std::strchr((char *)token, '.');
+	if (dec) {
+		*dec = '\0';
+		obj->value.number.fraction = atoi(dec + 1);
+	}
+	obj->value.number.integer = atoi(token);
+	return obj;
+}
+
+OBJECT * make_pointer(void * ptr) {
+	OBJECT *obj = _object_malloc(POINTER);
+	obj->value.ptr = ptr;
+	return obj;
+}
+
+OBJECT * make_string(const char *str, size_t length) {
+	OBJECT *obj = _object_malloc(STRING);
+	size_t len = strlen(str) + 1;
+	obj->size = length > len ? length : len;
+	//obj->value.string = GC_MALLOC(obj->size);
+	obj->value.string = static_cast<char *>(malloc(obj->size));
+	memcpy(obj->value.string, str, len);
+	return obj;
+}
+
+#define ERR(x) printf("%s:%d \n", __FILE__, __LINE__); debug(x);
+
+char * obj_inspector(OBJECT *obj) {
+	char *str = static_cast<char*>(malloc(256));
+	if (obj == NIL) {
+		snprintf(str, 255, "[%p NIL]", (void *)obj);
+		return str;
+	}
 	switch (object_type(obj)) {
-	case PAIR:   
-		{
-		os << "[" << (void *)obj << " "
-			<< (void *)_car(obj) << " "
-			<< (void *)_cdr(obj) << " "
-			<< (_cdr(obj) == NIL ? "(NIL)" : "", (void *)NIL)
-			<< "]";
-		break;
-		}
-	case SYMBOL: 
-		{
-			os << "[" << (void *)obj
-			<< (void *)obj << obj->value.symbol << "]"; 
-		}
-		break;
-	case STRING:
-		{ 
-			os << "[" << (void *)obj << " STRING " 
-			<<  obj->value.string
-			<< "]";
-		break;
-		}
-	case NUMBER:
-		{
-			os << "["
-			<< obj->value.number.integer << " "
-			<< obj->value.number.fraction << " "
-			<< "]";
-			break;
-		}
-	case OPERATOR: 
-	{
-		os << "["
-		<< ((void*)obj)
-		<< "OPERATOR ]";
-		break;
+	case PAIR:   snprintf(str, 255, "[%p, CONS %p %p %s] NIL=%p",
+		(void *)obj, (void *)_car(obj), (void *)_cdr(obj),
+		_cdr(obj) == NIL ? "(NIL)" : "",
+		(void *)NIL); break;
+	case SYMBOL: snprintf(str, 255, "[%p, SYMBOL %s]",
+		(void *)obj, obj->value.symbol); break;
+	case STRING: snprintf(str, 255, "[%p, STRING %s]",
+		(void *)obj, obj->value.string); break;
+	case NUMBER: snprintf(str, 255, "[%p, NUMBER %d.%d]",
+		(void *)obj, obj->value.number.integer, obj->value.number.fraction); break;
+	case OPERATOR: snprintf(str, 255, "[%p, OPERATOR ]",
+		(void*)obj); break;
+	default: abort();
 	}
-		
-	default: 
-		abort();
-	}
-
-    return os;
+	return str;
 }
-
 
 void indent_print_obj(OBJECT *obj, int indent) {
 	int i = 0;
-	for (i = 0; i < indent; i++) 
-		std::cout << " " << std::endl;
-	std::cout << obj << std::endl;
-	//printf("%s\n", obj_inspector(obj));
+	for (i = 0; i < indent; i++) printf("  ");
+	printf("%s\n", obj_inspector(obj));
 }
 
 OBJECT * debug(OBJECT *exp) {
@@ -439,14 +392,28 @@ next:
 	return NIL;
 }
 
+
+
 int main()
 {
 	//std::string filename = "init.lsp";
-	std::string filename = "small.lsp";
-	std::fstream s(filename);
+	//std::string filename = "small.lsp";
+	//std::fstream s(filename);
 
-	OBJECT *sexp = _read(s, 0);
-	debug(sexp);
+	OBJECT *port;
+	FILE *fp;
+	const char filename[] = "init.lsp";
+	int no_exit = 0; /* do not exit on EOF */
+	fp = fopen(filename, "r");
+	if (fp == NULL) 
+		return -1;
+	port = make_pointer(fp);
+
+	
+	OBJECT *exp = _read(port, no_exit);
+	debug(exp);
+	fclose(fp);
+
 	/*
 	std::string filename = "small.lsp";
 
